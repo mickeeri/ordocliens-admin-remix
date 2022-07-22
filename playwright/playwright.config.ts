7ae751173326abcd6ff044be70ed21f4e4125e32
Mikael Eriksson <micke_eri@hotmail.com>
2022-07-22 07:41:45 +0200

    Add .circleci/config.yml (#1)

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -39,8 +39,8 @@ const config: PlaywrightTestConfig = {
     baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'retain-on-failure',
-    video: 'on',
+    trace: 'off',
+    video: 'off',
   },
 
   /* Configure projects for major browsers */
